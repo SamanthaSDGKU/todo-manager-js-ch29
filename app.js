87e54d34f5eec186ae9b781a04328ad3554d7607
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2022-05-22 12:15:18 -0700

    scripting

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-function registerTask(){
-    //get the info from the input
+ //global vars
+let newTask = document.getElementById("txtTask");//travel the DOM once
+let count = 0;
 
+function registerTask(){   
     //display on console ---> in a html element
+    console.log(newTask.value);
+    let tmp=`<li id="${count}" class="task">${newTask.value} <button onclick="deleteTask(${count})">🗑️</button> </li>`;
+    document.getElementById("todoList").innerHTML+=tmp;
+    clearForm();
+    count++;
 }
 
-function deleteTask(){
+function deleteTask(x){
+    document.getElementById(x).remove();
+}
 
+function clearForm(){
+    newTask.value="";
 }
